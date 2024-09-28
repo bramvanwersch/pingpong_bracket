d77abb909240e35e2fefc80d9c9867f3d136f2fd
b.vanwersch <b.vanwersch@genomescan.nl>
2024-09-28 10:06:08 +0200

    some changes

--- chatting/consumers.py
+++ chatting/consumers.py
@@ -1,5 +1,13 @@
 import json
+from datetime import datetime
+
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
+
+from chatting.models import ChatMessage, UserMessage
+
+GENERAL_SESSION = 'general'
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -7,10 +15,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         room_name = self.scope["url_route"]["kwargs"]["chatname"]
         self.session_id = f"{room_name}"
 
-        await self.channel_layer.group_add(
-            self.session_id,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.session_id, self.channel_name)
 
         await self.accept()
 
@@ -25,14 +30,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
+        chat_message = await self._create_chat_message(message, username)
         await self.channel_layer.group_send(
             self.session_id, {
-                "type": "sendMessage",
-                "message": message,
-                "username": username,
+                "type": "send_message",
+                "message": chat_message.message,
+                "username": chat_message.sender.username,
+                "date": chat_message.date
             })
 
-    async def sendMessage(self, event):
+    async def send_message(self, event):
         message = event["message"]
         username = event["username"]
         await self.send(text_data=json.dumps({"message": message, "username": username}))
+
+    @database_sync_to_async
+    def _create_chat_message(self, message: str, sender: str) -> ChatMessage:
+        user = User.objects.get(username=sender)
+        chat_message = ChatMessage.objects.create(message=message, sender=user, date=datetime.now())
+        if self.session_id == GENERAL_SESSION:
+            pass
+        else:
+            other_user = User.objects.get(username=self.session_id)
+            UserMessage.objects.create(message=chat_message, message_read=False, user=other_user)
+            UserMessage.objects.create(message=chat_message, message_read=True, user=user)
+        return chat_message
--- chatting/views.py
+++ chatting/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models.functions import Lower
 from django.template.response import TemplateResponse
 from django.views import View
 
@@ -6,7 +7,7 @@ from django.views import View
 class ChatRoomView(View):
 
     def get(self, request, room_name: str = "general"):
-        names = {u.username for u in User.objects.all()}
-        names.remove(request.user.username)
+        names = {u.username: f"u_{u.pk}" for u in User.objects.all().order_by(Lower('username'))}
+        del names[request.user.username]
         return TemplateResponse(request, "chatroom.html",
-                                {"current": "chatting", "names": sorted(names, key=lambda x: x.lower()), "room_name": room_name})
+                                {"current": "chatting", "names": names, "room_name": room_name})
