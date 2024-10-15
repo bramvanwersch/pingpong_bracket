fa55a99207debae9b45d4ba60f40d1149d89b219
b.vanwersch <b.vanwersch@genomescan.nl>
2024-10-15 08:05:15 +0200

    revenge!

--- chatting/views.py
+++ chatting/views.py
@@ -3,7 +3,7 @@ from django.db.models.functions import Lower
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 
-from chatting.models import ChatGroup, ChatMessage, UserMessage
+from chatting.models import ChatGroup, ChatGroupUser, ChatMessage, UserMessage
 from chatting.src import utility
 from general_src.base_view import BaseView
 
@@ -12,6 +12,10 @@ class ChatRoomView(BaseView):
     def get(self, request, group_id: int = None):
         names = utility.get_user_mapping([request.user])
         group_data = utility.get_group_data(request.user)
+        if group_id is not None:
+            user_ids = {cgu.user.pk for cgu in ChatGroupUser.objects.filter(group_id=group_id)}
+            if request.user.pk not in user_ids:
+                return TemplateResponse(request, "403.html")
         messages = ChatMessage.objects.filter(chat_group_id=group_id).order_by("date")
         # read all the messages when opening a chat
         UserMessage.objects.filter(message__chat_group_id=group_id, user=request.user, message_read=False).update(
