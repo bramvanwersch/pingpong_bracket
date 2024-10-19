718ce5acac8817c8f1c00ca16d8491d38f915088
b.vanwersch <b.vanwersch@genomescan.nl>
2024-10-19 13:56:32 +0200

    limited the image upload format

--- login/api.py
+++ login/api.py
@@ -1,12 +1,24 @@
+import os
+
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 
 from login.models import UserData
 
+ALLOWED_IMAGE_FORMATS = {"jpg", "png", "gif", "webm", "webp", "apng", "svg", "avif"}
+
 
 class UploadLoginImage(APIView):
     def post(self, request):
         user_data = UserData.objects.get(user=request.user)
+        file = request.FILES["file"]
+        try:
+            extension = file.name.rsplit(".", 1)[1]
+        except IndexError:
+            return HttpResponse("Unsupported image format", status=400)
+        if extension not in ALLOWED_IMAGE_FORMATS:
+            return HttpResponse("Unsupported image format", status=400)
+        os.remove(user_data.profile_picture.path)
         user_data.profile_picture = request.FILES["file"]
         user_data.save()
         return HttpResponse()
